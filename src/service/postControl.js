fbfa615619bea90ed39ba5c357e043b98340e7b1
Cristina <20213tn151@utez.edu.mx>
2024-02-02 16:54:04 -0600

    [UPDATE](●'◡'●)

--- src/service/postControl.js
+++ src/service/postControl.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'http://localhost:8080/api/vehiculos/';
 
-const registrarCoche = async (brand, model, year, serie) => {
+const registrarCoche = async (form) => {
   const headers = {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*'
   };
 
-  const data = { brand, model, year, serie };
+  const data = { form };
 
   try {
     const response = await axios.post(API_URL, data, { headers });
